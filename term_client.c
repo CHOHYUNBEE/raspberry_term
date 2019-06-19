88e74c52f2425844c601f3c4a9c0ddaa7bdf24d0
hyunbi <vvv3349@naver.com>
2019-06-19 16:58:43 +0900

    .

--- term_client.c
+++ term_client.c
@@ -23,8 +23,15 @@
 #define LED_GREEN 21 // 5
 #define LED_BLUE 22   //6
 #define BUZZER 15
- 
-void * send_message(void *arg);
+
+typedef struct senddata
+{
+    int passok;
+    char message[BUFSIZE];
+    int flag;
+}s_data;
+
+s_data send_message(int sock, s_data sData);
 void * recv_message(void *arg);
 void error_handling(char *message);
 void wiringPi_Init();
@@ -33,7 +40,7 @@ void Detect_Color( char c);
 void PIR_interrupt();
 void *Thread_Func();
 void *buzzer();
-void *led();
+void *led(void *arg);
 void *pir();
  
 char message[BUFSIZE];
@@ -46,113 +53,131 @@ int i = 0;
 unsigned int countR = 0, countG = 0, countB = 0;
 unsigned int counter = 0;
 
-typedef struct senddata
-{
-  int passok;
-  char message;
-  int flag;
-}s_data;
-
 int main(int argc, char **argv)
 {
-  if(wiringPiSetup () == -1)
-    exit(1);
-  int sock;
-  struct sockaddr_in serv_addr;
-  pthread_t snd_thread, rcv_thread;
-  void * thread_result;
-  if(argc!=3){
-    printf("Usage : %s <IP> <port>\n", argv[0]);
-    exit(1);
-  }
- 
-  sock=socket(PF_INET, SOCK_STREAM, 0);
-  if(sock==-1)
-     error_handling("socket() error");
-  memset(&serv_addr, 0, sizeof(serv_addr));
-  serv_addr.sin_family=AF_INET;
-  serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-  serv_addr.sin_port=htons(atoi(argv[2]));
-  
-  if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-    error_handling("connect() error");
-  pthread_create(&snd_thread, NULL, send_message, (void*)sock);
-  pthread_create(&rcv_thread, NULL, recv_message, (void*)sock);
-  pthread_join(snd_thread, &thread_result);
-  pthread_join(rcv_thread, &thread_result);
-  close(sock);  return 0;
-}
-void * send_message(void *arg) /* 메시지 전송 쓰레드 실행 함수 */
-{
-    int sock = (int)arg;
-    s_data sdata;
-    memset(&sdata, 0, sizeof(s_data));
-  
-  
-    pthread_t thread_led, thread_pir, thread_buzzer, thread_color;
+    if(wiringPiSetup () == -1) exit(1);
 
-    pthread_create(&thread_pir, NULL, pir, NULL);
-    pthread_create(&thread_led, NULL, led, NULL);
-    pthread_create(&thread_buzzer, NULL, buzzer, NULL);
-    pthread_create(&thread_color, NULL, Thread_Func, NULL);
+    int sock;
+    struct sockaddr_in serv_addr;
+
+    pthread_t rcv_thread;
+    pthread_t thread_led, thread_pir, thread_buzzer, thread_color;
+    void * thread_result;
+
+    if(argc!=3){
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(sock==-1) {
+        error_handling("socket() error");
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+    serv_addr.sin_port=htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) {
+        error_handling("connect() error");
+    }
 
     wiringPi_Init();
 
-    while(1) {
-        sdata.flag = 0;
-        sdata.passok = passok;
-        sdata.message = "Warning";
+    pthread_create(&rcv_thread, NULL, recv_message, (void*)sock);
+    pthread_create(&thread_pir, NULL, pir, NULL);
+    pthread_create(&thread_led, NULL, led, (void*)sock);
+    pthread_create(&thread_buzzer, NULL, buzzer, NULL);
+    pthread_create(&thread_color, NULL, Thread_Func, NULL);
 
-        write(sock,(char*)&sdata,sizeof(sdata));
+    pthread_join(rcv_thread, &thread_result);
+    pthread_join(thread_pir, &thread_result);
+    pthread_join(thread_led, &thread_result);
+    pthread_join(thread_buzzer, &thread_result);
+    pthread_join(thread_color, &thread_result);
 
-        memset(message, 0, BUFSIZE);
-        str_len = read(sock, message, sizeof(message));
-        message[str_len] ="\0";
-        s_data = (senddata*)message;
-        sleep(2);
-        if(s_data.flag == 0)
-        {
+    close(sock);
+    return 0;
+}
 
-        }
-        else if(s_data.flag ==1) {
-            message[0] = '\0';
-            memset(message, 0x00, BUFSIZE);
-            strcpy(message, "\n");
-            fflush(stdin);
-            fgets(message, sizeof(message), stdin);
-        }
-        else if(s_data.flag==2)
-        {
-            pthread_create(&thread_pir, NULL, pir, NULL);
-            pthread_create(&thread_led, NULL, led, NULL);
-            pthread_create(&thread_buzzer, NULL, buzzer, NULL);
-            wiringPi_Init();
-            pthread_create(&thread_color, NULL, Thread_Func, NULL);
-        }
-    }
+s_data send_message(int sock, s_data sData) /* 메시지 전송 쓰레드 실행 함수 */
+{
+    write(sock,(void*)&sdata,sizeof(sdata));
+
+    s_data recv_data; memset(&recv_data, 0, sizeof(s_data));
+    read(sock, (void*)&recv_data, sizeof(recv_data));
+
+    printf("%s\n", recv_data.message);
+    memset(recv_data.message, 0, BUFSIZE);
+    fgets(recv_data.message, BUFSIZE, stdin);
+
+    write(sock,(void*)&recv_data,sizeof(send_data));
+
+    memset(&sData, 0, sizeof(sData));
+    read(sock, (void*)&sData, sizeof(sData));
+
+    return sData;
+//    while(1) {
+//        sdata.flag = 0;
+//        sdata.passok = passok;
+//        strcpy(sdata.message, "Warning");
+//
+//        write(sock,(char*)&sdata,sizeof(sdata));
+//
+//        memset(message, 0, BUFSIZE);
+//
+//        int str_len = read(sock, message, sizeof(message));
+//        message[str_len] ="\0";
+//
+//        char client_msg[BUFSIZE] =
+//
+//        sleep(2);
+//        if(s_data.flag == 0)
+//        {
+//
+//        }
+//        else if(s_data.flag ==1) {
+//            message[0] = '\0';
+//            memset(message, 0x00, BUFSIZE);
+//            strcpy(message, "\n");
+//            fgets(message, sizeof(message), stdin);
+//        }
+//        else if(s_data.flag==2)
+//        {
+//            pthread_create(&thread_pir, NULL, pir, NULL);
+//            pthread_create(&thread_led, NULL, led, NULL);
+//            pthread_create(&thread_buzzer, NULL, buzzer, NULL);
+//            wiringPi_Init();
+//            pthread_create(&thread_color, NULL, Thread_Func, NULL);
+//        }
+//    }
 }
  
 void * recv_message(void *arg) /* 메시지 수신 쓰레드 실행 함수 */
 {
-  int sock = (int)arg;
-  //char send_message[BUFSIZE];
-  int str_len;
-  while(1){
-    str_len = read(sock, send_message, BUFSIZE-1);
-     //if(str_len==-1) return 1;
-    //send_message[str_len]=0;
-    //fputs(send_message, stdout);
-  }
+//  int sock = (int)arg;
+//  //char send_message[BUFSIZE];
+//  int str_len;
+//  while(1){
+//    str_len = read(sock, send_message, BUFSIZE-1);
+//     //if(str_len==-1) return 1;
+//    //send_message[str_len]=0;
+//    //fputs(send_message, stdout);
+//  }
 }
+
 void error_handling(char *message)
 {
   fputs(message, stderr);
   fputc('\n', stderr);
   exit(1);
 }
+
 void Count(){
-counter ++;
+    counter ++;
 }
+
 void wiringPi_Init(){
    pinMode(COLOR_S0,OUTPUT);
    pinMode(COLOR_S1,OUTPUT);
@@ -167,8 +192,9 @@ void wiringPi_Init(){
    digitalWrite (COLOR_S0, 0);
    digitalWrite(COLOR_S1, 1);
    digitalWrite(COLOR_LED, 1);
-wiringPiISR(COLOR_OUT,INT_EDGE_RISING,&Count);
+   wiringPiISR(COLOR_OUT,INT_EDGE_RISING,&Count);
 }
+
 void *pir()
 {
     pinMode(PIR_D, INPUT);
@@ -183,108 +209,123 @@ void *pir()
        }
   }
 }
-void *led()
-{ 
-   senddata s_data;
-   pinMode(LED_RED,OUTPUT);
-   pinMode(LED_GREEN,OUTPUT);
-   pinMode(LED_BLUE,OUTPUT);
-   while(1)
-  {
-   if(countR>500 && countR>countG && countR>countB)
-   {
-      //통과 
-    passok = 0;
-    digitalWrite(LED_RED,0);
-    digitalWrite(LED_BLUE,0);
-    digitalWrite(LED_GREEN,1);
-    count_p=0;
-    pir_flag = 0;
-    sleep(5);
-   }
-   else if(count_p>=5)
-   {
-    passok=1;
-    digitalWrite(LED_RED,1);
-    digitalWrite(LED_GREEN,0);
-    digitalWrite(LED_BLUE,0);
-   }
-  else if(s_data.flag==2)
-  {
-    digitalWrite(LED_RED,0);
-    digitalWrite(LED_BLUE,1);
-    digitalWrite(LED_GREEN,0);
-  }
-   else
-   {
-    digitalWrite(LED_RED,0);
-    digitalWrite(LED_BLUE,1);
-    digitalWrite(LED_GREEN,0);
-   }
-  }
+
+void *led(void *arg)
+{
+    pinMode(LED_RED,OUTPUT);
+    pinMode(LED_GREEN,OUTPUT);
+    pinMode(LED_BLUE,OUTPUT);
+
+    while(1)
+    {
+        sleep(1);
+
+        if(countR>500 && countR>countG && countR>countB)
+        {
+            // admin card
+            passok = 0;
+            digitalWrite(LED_RED,0);
+            digitalWrite(LED_BLUE,0);
+            digitalWrite(LED_GREEN,1);
+            count_p = 0;
+            pir_flag = 0;
+            sleep(5);
+        }
+        else if(count_p>=5)
+        {
+            // warning
+            passok=1;
+
+            digitalWrite(LED_RED,1);
+            digitalWrite(LED_GREEN,0);
+            digitalWrite(LED_BLUE,0);
+
+            s_data sData; memset(&sData, 0, sizeof(s_data));
+            strcpy(sData, "Warning");
+            send_message((int)arg, sData);
+
+            printf("sdata.flag = %d\n", sData.flag);
+            printf("sdata.msg = %s\n", sData.message);
+
+        }
+        else if(sData.flag == 2)
+        {
+            // nomal
+            digitalWrite(LED_RED,0);
+            digitalWrite(LED_BLUE,1);
+            digitalWrite(LED_GREEN,0);
+        }
+        else
+        {
+            // nomal
+            digitalWrite(LED_RED,0);
+            digitalWrite(LED_BLUE,1);
+            digitalWrite(LED_GREEN,0);
+        }
+    }
 }
+
 void *buzzer()
 {
     while(1){
-    if(passok==0)
-    {
-      digitalWrite(BUZZER,0);
+        if(passok==0)
+        {
+            digitalWrite(BUZZER,0);
+        }
+        else if(passok==1)
+        {
+            while(passok!=0)
+            {
+                digitalWrite(BUZZER,1);
+                sleep(1);
+                digitalWrite(BUZZER,0);
+                sleep(2);
+            }
+        }
     }
-    else if(passok==1)
-    {
-      while(passok!=0)
-      {
- 
-       digitalWrite(BUZZER,1);
-       sleep(1);
-       digitalWrite(BUZZER,0);
-       sleep(2);
- 
-      }
-    }
-  }
 }
+
 void Detect_Color( char c)
 {
-if(c==0) { // RED
-digitalWrite(COLOR_S2, 0);
-digitalWrite(COLOR_S3, 0);
-}
-else if(c==1) { //GREEN
-digitalWrite(COLOR_S2, 1);
-digitalWrite(COLOR_S3, 1);
-}
-else if(c==2) { //BLUE
-digitalWrite(COLOR_S2, 0);
-digitalWrite(COLOR_S3, 1);
-}
+    if(c==0) { // RED
+        digitalWrite(COLOR_S2, 0);
+        digitalWrite(COLOR_S3, 0);
+    }
+    else if(c==1) { //GREEN
+        digitalWrite(COLOR_S2, 1);
+        digitalWrite(COLOR_S3, 1);
+    }
+    else if(c==2) { //BLUE
+        digitalWrite(COLOR_S2, 0);
+        digitalWrite(COLOR_S3, 1);
+    }
 }
  
 void PIR_interrupt() 
 {
-pir_flag = 1 ;
+    pir_flag = 1 ;
 }
  
  
 void *Thread_Func(){
-while(1){ 
-   if(color==0) { //RED
-          countR = counter ;
-      color = 1; //Request Green
-      Detect_Color(color); //S2 =1, S3 = 1
-   }
-   else if(color==1) { //Green
-          countG = counter ;
-      color = 2; // Request Blue
-      Detect_Color(color); //S2 = 0, S3 =1
-   }
-   else if(color==2) { //BLEU
-          countB = counter ;
-      color = 0; //Request Red
-      Detect_Color(color);//S2 = 0, S3 = 0
-   }
-   counter = 0 ;
-   delay(1000);
-   }
+    while(1){
+        if(color==0) { //RED
+            countR = counter ;
+            color = 1; //Request Green
+            Detect_Color(color); //S2 =1, S3 = 1
+        }
+        else if(color==1) { //Green
+            countG = counter ;
+            color = 2; // Request Blue
+            Detect_Color(color); //S2 = 0, S3 =1
+        }
+        else if(color==2) { //BLEU
+            countB = counter ;
+            color = 0; //Request Red
+            Detect_Color(color);//S2 = 0, S3 = 0
+        }
+        counter = 0 ;
+        delay(1000);
+    }
 }
  
--- term_server.c
+++ term_server.c
@@ -8,108 +8,128 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #define BUFSIZE 100
+
 void * clnt_connection(void *arg);
 void send_message(char * message, int len);
 void error_handling(char *message);
 int clnt_number = 0;
 int clnt_socks[10];
 pthread_mutex_t mutx;
+
 typedef struct senddata
 {
    int passok; //통과유무
-   char message; 
+   char message[BUFSIZE];
    int flag; //행동 플래그
 }s_data;
+
 int main(int argc, char **argv)
 {
-   int serv_sock, clnt_sock;
-   struct sockaddr_in serv_addr;
-   struct sockaddr_in clnt_addr;
-   int clnt_addr_size;
-   pthread_t thread;
-   if (argc != 2) {
-      printf("Usage : %s <port> \n", argv[0]);
-      exit(1);
-   }
-   if (pthread_mutex_init(&mutx, NULL))
-      error_handling("mutex init error");
-   serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-   memset(&serv_addr, 0, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-   serv_addr.sin_port = htons(atoi(argv[1]));
-   if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
-      error_handling("bind() error");
-   if (listen(serv_sock, 5) == -1)
-      error_handling("listen() error");
-   while (1) {
-      clnt_addr_size = sizeof(clnt_addr);
-      clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-      pthread_mutex_lock(&mutx);
-      clnt_socks[clnt_number++] = clnt_sock;
-      pthread_mutex_unlock(&mutx);
-      pthread_create(&thread, NULL, clnt_connection, (void*)clnt_sock);
-      printf("새로운 연결, 클라이언트 IP : %s \n", inet_ntoa(clnt_addr.sin_addr));
-   }
-   return 0;
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    int clnt_addr_size;
+    pthread_t thread;
+
+    if (argc != 2) {
+        printf("Usage : %s <port> \n", argv[0]);
+        exit(1);
+    }
+
+    if (pthread_mutex_init(&mutx, NULL)) {
+        error_handling("mutex init error");
+    }
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("bind() error");
+    }
+
+    if (listen(serv_sock, 5) == -1) {
+        error_handling("listen() error");
+    }
+
+    while (1) {
+        clnt_addr_size = sizeof(clnt_addr);
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+
+        pthread_mutex_lock(&mutx);
+        clnt_socks[clnt_number++] = clnt_sock;
+        pthread_mutex_unlock(&mutx);
+
+        pthread_create(&thread, NULL, clnt_connection, (void*)clnt_sock);
+        printf("새로운 연결, 클라이언트 IP : %s \n", inet_ntoa(clnt_addr.sin_addr));
+    }
+    return 0;
 }
+
 void * clnt_connection(void *arg)
 {
-   int clnt_sock = (int)arg;
-   struct senddata s_data;
-   int str_len = 0;
-   s_data.flag = 0;
-   char message[BUFSIZE];
-   char sendmsg[BUFSIZE];
-   int i;
-   while ((str_len = read(clnt_sock, s_data.message, sizeof(s_data))) != 0) {
-      fflush(stdin);
-      printf("[Client] %s\n", s_data.message);
-      if (s_data.flag == 0) { //경고 모드(침입자 발생)
-         if (strcmp(s_data.message, "Warning\n") == 0) {
-            strcpy(sendmsg, "Warning message if you mistake, you enter this number /1457/\n");
-            send_message(sendmsg, strlen(sendmsg));
-            s_data.flag = 1; 
-            write(clnt_sock, (char*)&s_data, sizeof(s_data));
-         }
-      }
-      else if (s_data.flag == 1) { //보안코드 확인 모드
-         if (strcmp(message, "1457\n") == 0) { //경고 모드 해제
-            strcpy(sendmsg, "Undo Warning Mode\n");
-            send_message(sendmsg, strlen(sendmsg));
-            s_data.flag = 2;
-            write(clnt_sock, (char*)&s_data, sizeof(s_data));
-         }
-         else{ //잘못된 보안코드 -> 다시 경고 모드로
-            strcpy(sendmsg, "Invalid security code\n");
-            send_message(sendmsg, strlen(sendmsg));            
-            s_data.flag = 0;
-            write(clnt_sock, (char*)&s_data, sizeof(s_data));
-         }
-      }
-      else if(s_data.flag == 2){ //보안 카드가 입력됨 -> 보안 해제 
-         strcpy(sendmsg, "Security card entered. Unsecure\n");
-         send_message(sendmsg, strlen(sendmsg));
-         s_data.flag = 2;
-         write(clnt_sock, (char*)&s_data, sizeof(s_data));
-      }
-      send_message(message, str_len);
-      memset(message, 0x00, BUFSIZE);
-      memset(sendmsg, 0x00, BUFSIZE);
-      fflush(stdin);
-   }
-   pthread_mutex_lock(&mutx);
-   for (i = 0; i < clnt_number; i++) { /* 클라이언트 연결 종료 시 */
+    int clnt_sock = (int)arg;
+    s_data sData; memset(&sData, 0, sizeof(s_data));
+    int str_len = 0;
+    char message[BUFSIZE] = "";
+    char sendmsg[BUFSIZE] = "";
+    int i;
+
+    while ((str_len = read(clnt_sock, (void*)&sData, sizeof(sData))) != 0) {
+        fflush(stdout);
+        printf("[Client] %s\n", sData.message);
+        if (sData.flag == 0) { //경고 모드(침입자 발생)
+            if (strcmp(sData.message, "Warning") == 0) {
+                strcpy(sendmsg, "Warning message if you mistake, you enter this number /1457/\n");
+                sData.flag = 1;
+
+                write(clnt_sock, (void*)&sData, sizeof(sData));
+            }
+        }
+        else if (sData.flag == 1) { //보안코드 확인 모드
+            if (strcmp(message, "1457\n") == 0) { //경고 모드 해제
+                memset(sData.message, 0, BUFSIZE);
+                strcpy(sData.message, "Undo Warning Mode");
+                sData.flag = 2;
+
+                write(clnt_sock, (void*)&sData, sizeof(sData));
+            }
+            else{ //잘못된 보안코드 -> 다시 경고 모드로
+                memset(sData.message, 0, BUFSIZE);
+                strcpy(sData.message, "Invalid security code");
+                sData.flag = 0;
+                write(clnt_sock, (void*)&sData, sizeof(sData));
+            }
+        }
+        else if(sData.flag == 2){ //보안 카드가 입력됨 -> 보안 해제
+            memset(sData.message, 0, BUFSIZE);
+            strcpy(sData.message, "Security card entered. Unsecure");
+            sData.flag = 2;
+            write(clnt_sock, (void*)&sData, sizeof(sData));
+        }
+
+//        send_message(message, str_len);
+        memset(message, 0x00, BUFSIZE);
+        memset(sendmsg, 0x00, BUFSIZE);
+        memset(&sData, 0, sizeof(s_data));
+        fflush(stdout);
+    }
+
+    pthread_mutex_lock(&mutx);
+    for (i = 0; i < clnt_number; i++) { /* 클라이언트 연결 종료 시 */
       if (clnt_sock == clnt_socks[i]) {
          for (; i < clnt_number - 1; i++)
             clnt_socks[i] = clnt_socks[i + 1];
          break;
       }
-   }
-   clnt_number--;
-   pthread_mutex_unlock(&mutx);
-   close(clnt_sock);
-   return 0;
+    }
+    clnt_number--;
+    pthread_mutex_unlock(&mutx);
+
+    close(clnt_sock);
+    return 0;
 }
 void send_message(char * message, int len)
 {
@@ -122,7 +142,7 @@ void send_message(char * message, int len)
 void error_handling(char *message)
 {
    fputs(message, stderr);
-   fputc(' \n', stderr);
+   fputc('\n', stderr);
    exit(1);
 }
  
